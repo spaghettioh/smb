a75902e923286f7b934db7945862616001131b86
Eric Mofield <ericmofield@gmail.com>
2018-12-21 08:35:45 -0500

    Create a coin prefab and test it in the level.

--- Assets/Scripts/CoinGet.cs
+++ Assets/Scripts/CoinGet.cs
@@ -2,21 +2,14 @@
 
 public class CoinGet : MonoBehaviour {
 
-    public IntVariable Score;
-
     Animator anim;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "Player")
         {
