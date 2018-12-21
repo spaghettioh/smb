303ea7bb7d9d3075e697584e8462f30cb276644c
Eric Mofield <ericmofield@gmail.com>
2018-12-21 08:24:09 -0500

    Test out some coin collision setup.

---
+++ Assets/Scripts/AnimationEventListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AnimationEventListener : MonoBehaviour {
+    public IntVariable Score;
+
+    void AnimAdjustScore(int amount)
+    {
+        Score.ApplyChange(amount);
+    }
+
+    void AnimDestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CoinGet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CoinGet : MonoBehaviour {
+
+    public IntVariable Score;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            anim.SetTrigger("CoinGet");
+        }
+    }
+}
