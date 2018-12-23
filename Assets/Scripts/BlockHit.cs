398d6fb1d130beb5206d374404e2c0124b0b20f4
Eric Mofield <ericmofield@gmail.com>
2018-12-23 17:23:44 -0500

    Add a brick prefab.

---
+++ Assets/Scripts/BlockHit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BlockHit : MonoBehaviour
+{
+    public bool isQuestionBlock;
+    public GameObject label;
+    public UnityEvent hit;
+
+    //Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //anim = gameObject.GetComponent<Animator>();
+        Destroy(label);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            hit.Invoke();
+        }
+    }
+
+    public void DestoryThis(GameObject g)
+    {
+        StartCoroutine(DestroyNow(g));
+    }
+
+    IEnumerator DestroyNow(GameObject g)
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(g);
+    }
+}
