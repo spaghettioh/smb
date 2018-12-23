b8847a755312d5f03e14a00fcaf3d82fad679e1d
Eric Mofield <ericmofield@gmail.com>
2018-12-23 10:55:02 -0500

    Create level boundaries prefab.

---
+++ Assets/PitDeath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PitDeath : MonoBehaviour
+{
+    public FloatVariable playerSize;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerSize.ApplyChange(-5);
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
