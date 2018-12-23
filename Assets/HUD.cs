4f20d9f34e1d6cfdc74ff3c06d0869dea5affb01
Eric Mofield <ericmofield@gmail.com>
2018-12-23 01:02:43 -0500

    Add a HUD.

---
+++ Assets/HUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text scoreText;
+    public IntVariable scoreValue;
+
+    public Text coinsText;
+    public IntVariable coinsValue;
+
+    public Text livesText;
+    public IntVariable livesValue;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = scoreValue.Value.ToString();
+        coinsText.text = coinsValue.Value.ToString();
+        livesText.text = livesValue.Value.ToString();
+    }
+}
