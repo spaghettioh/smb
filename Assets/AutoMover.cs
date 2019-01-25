5ee449fab9a14efbba64fbca8d9eebd7d7259932
Eric Mofield <ericmofield@gmail.com>
2019-01-25 00:03:50 -0500

    Update automover and mushroom.

--- Assets/AutoMover.cs
+++ Assets/AutoMover.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoMover : MonoBehaviour {
+    public int moveSpeed = 5;
+    public bool moveRight = true;
+    private int moveDirection;
+    public Rigidbody2D body;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    private void Start()
+    {
+        if (moveRight)
+            moveDirection = 1;
+        else
+            moveDirection = -1;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        RaycastHit2D turnAround = Physics2D.Raycast(transform.position, new Vector2(moveDirection, 0));
+        body.velocity = new Vector2(moveDirection * moveSpeed, body.velocity.y);
+
+        // Bounce off objects
+        if (turnAround.distance < .4f)
+            moveDirection *= -1;
 	}
 }
