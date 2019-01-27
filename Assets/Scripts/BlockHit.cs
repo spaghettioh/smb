c30741ec23a843bb559706d3a6d05098e0d6e489
Eric Mofield <ericmofield@gmail.com>
2019-01-27 00:21:25 -0500

    Optimize Coin block.

--- Assets/Scripts/BlockHit.cs
+++ Assets/Scripts/BlockHit.cs
@@ -4,25 +4,38 @@ using UnityEngine.Events;
 
 public class BlockHit : MonoBehaviour
 {
-    public bool isItemBlock;
-    public GameObject label;
-    public UnityEvent hit;
     public FloatVariable playerSize;
+    public bool isQuestionBlock;
+    public GameObject label;
+    public UnityEvent activate;
+    public UnityEvent bump;
 
-  	// Use this for initialization
-	void Start ()
+    bool activated = false;
+
+    // Use this for initialization
+    void Start()
     {
+        // Remove the editor label
         Destroy(label);
-	}
+    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void FixedUpdate()
     {
-        if (collision.name == "Player")
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, -1));
+
+        // Record the hit
+        if (hit.distance < .55f && hit.collider.name == "Player")
         {
-            if (playerSize.Value > 0 || isItemBlock)
+            if (!activated && (playerSize.Value > 0 || isQuestionBlock))
             {
-                hit.Invoke();
+                activate.Invoke();
+                activated = true;
             }
+            else
+            {
+                bump.Invoke();
+            }
+
         }
     }
 
